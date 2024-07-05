bad7e2aa8b0ecab25394d9d7921a58df975605d8
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-07-05 22:33:01 +0600

    Completed frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Header from './pages/Header/Header';
+import Home from './pages/Home/Home';
+import Skills from './pages/Skills/Skills';
+import Work from './pages/Work/Work';
+import './App.css';
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
+      <div className="main">
+        <Home />
+        <About />
+        <Skills />
+        <Contact />
+        <Work />
+      </div>
     </>
   );
 }
